e4e67be7c4a2d5b789531fdec9df9fa8c5475733
huang <540671662@qq.com>
2018-08-12 21:24:16 +0800

    地图

--- code/cocosmanager/src/main/java/com/boot/Application.java
+++ code/cocosmanager/src/main/java/com/boot/Application.java
@@ -3,13 +3,13 @@ package com.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.boot"})
 public class Application {
-      public static void main(String[] args) {
-          SpringApplication.run(Application.class, args);
-      }
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
 
 }
